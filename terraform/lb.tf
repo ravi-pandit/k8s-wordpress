0115aa364bb95548f787899f42e1f00a74b2df6a
Ravi P <rrp@narola.email>
2023-04-23 00:21:30 +0530

    add new pipeline file

---
+++ terraform/lb.tf
@@ -0,0 +1,32 @@
+resource "aws_lb_target_group" "wordpress-tg" {
+  name_prefix        = "wordpress-tg"
+  port               = 80
+  protocol           = "HTTP"
+  vpc_id             = aws_vpc.demo_vpc.id
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 30
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+    path                = "/"
+  }
+}
+
+
+resource "aws_lb_target_group" "green-tg" {
+  name_prefix        = "green-tg"
+  port               = 80
+  protocol           = "HTTP"
+  vpc_id             = aws_vpc.demo_vpc.id
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 30
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+    path                = "/green/index.html"
+  }
+}
